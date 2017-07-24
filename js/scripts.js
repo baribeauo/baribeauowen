e6c7973626e1e93a3d6beeaea1a38725128f85b6
baribeauo <baribeauo@gmail.com>
2017-07-24 12:23:09 -0400

    Extensive Site expansion

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
+
+
 var hexArray = ['#f44242','#27ce8e', '#32b4ff', '#db29c3']
 var randomColor = hexArray[Math.floor(Math.random() * hexArray.length)];
 
 $(".colourize").css("color",randomColor); 
+
+$('.article-list-title').hover(
+       function(){ $(this).css("color", randomColor) }
+)
+$('.article-list-title').mouseleave(
+       function(){ $(this).css("color", "black") }
+)
