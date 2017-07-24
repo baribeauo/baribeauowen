b6c28953f4dc9fe29eb17253852f2d32db379a34
baribeauo <baribeauo@gmail.com>
2017-07-24 15:49:17 -0400

    created functional site navigation, landing page

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var hexArray = ['#f44242','#27ce8e', '#32b4ff', '#db29c3']
 var randomColor = hexArray[Math.floor(Math.random() * hexArray.length)];
 
 $(".colourize").css("color",randomColor); 
+$(".spanner, .circle").css("background-color",randomColor); 
 
 $('.article-list-title').hover(
        function(){ $(this).css("color", randomColor) }
